3c9789b643a0d987ec0c239f2d54a97f89b56743
fodderaccount <tiachop1699@gmail.com>
2022-08-08 23:22:55 +0700

    login redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,8 +27,15 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    //protected $redirectTo = RouteServiceProvider::HOME;
+    protected function authenticated(Request $request, $user)
+    {
+        if($user->role_as=='1'){
+            return redirect('/home');
+        }else{
+            return redirect('/');
+        }
+    }
     /**
      * Create a new controller instance.
      *
@@ -38,9 +46,9 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function redirectTo()
-    {
-        session()->flash('success', 'You are logged in!');
-        return $this->redirectTo;
-    }
+    // protected function redirectTo()
+    // {
+    //     session()->flash('success', 'You are logged in!');
+    //     return $this->redirectTo;
+    // }
 }
