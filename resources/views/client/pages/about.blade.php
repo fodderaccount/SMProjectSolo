168159d9efbd8d27ef6ae4dbcadff2f4e5b0825e
SonNC2503 <sonleo1102@gmail.com>
2022-08-11 23:20:23 +0700

    Create table, controller, model of categories

---
+++ resources/views/client/pages/about.blade.php
@@ -0,0 +1,40 @@
+@extends('client.layouts.master')
+
+@section('content')
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading">about us</h1>
+ 
+    <div class="box-container">
+ 
+       <div class="box">
+          <img src="client/images/about-1.svg" alt="">
+          <h3>made with love</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum quae amet beatae magni numquam facere sit. Tempora vel laboriosam repudiandae!</p>
+          <a href="#menu" class="btn">our menu</a>
+       </div>
+ 
+       <div class="box">
+          <img src="client/images/about-2.svg" alt="">
+          <h3>30 minutes delivery</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum quae amet beatae magni numquam facere sit. Tempora vel laboriosam repudiandae!</p>
+          <a href="#menu" class="btn">our menu</a>
+       </div>
+ 
+       <div class="box">
+          <img src="client/images/about-3.svg" alt="">
+          <h3>share with freinds</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum quae amet beatae magni numquam facere sit. Tempora vel laboriosam repudiandae!</p>
+          <a href="#menu" class="btn">our menu</a>
+       </div>
+ 
+    </div>
+ 
+ </section>
+ 
+ <!-- about section ends -->
+
+ @endsection
