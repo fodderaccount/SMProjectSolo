bcba3c28bb356263d6280dd4c8c88f37f993b18d
SonNC2503 <sonleo1102@gmail.com>
2022-08-13 14:23:07 +0700

    edit and delete product success => product finish

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -35,8 +36,49 @@ class ProductController extends Controller
             $product->description=$request->input('description');
             $product->category_id=$request->input('category_id');
             $product->save();
-            return redirect()->route('products.index')->with('success', 'Add New Product Successfully!');
-        
+            return redirect()->route('products.index')->with('success', 'Add New Product Successfully!');     
+    }  
+
+    public function getEditProduct($id){
+        $categories=Category::all();
+        $data['product']=Product::find($id);
+        return view('pages.editProduct', $data,compact('categories'));
     }
-    
+
+    public function postEditProduct(Request $request,$id){
+        $product=Product::find($id);
+        if($request->hasFile('image'))
+        {
+            $path='uploads/img/'.$product->image;
+            if(File::exists($path))
+            {
+                File::delete($path);
+            }
+
+            $file=$request->file('image');                
+            $ext=$file->getClientOriginalExtension();
+            $filename=time().'.'.$ext;
+            $file->move('uploads/img/', $filename);
+            $product->image=$filename;
+            
+        }
+            
+        $product->name=$request->input('name');
+        $product->price=$request->input('price');
+        $product->description=$request->input('description');
+        //$product->category_id=$request->input('category_id');
+        $product->save();
+        return redirect()->route('products.index')->with('success', 'Product Updated Successfully!');
+    }
+    public function deleteProduct($id){
+        $product =Product::find($id);
+        $path = 'uploads/img/'.$product->image;
+        if(File::exists($path))
+        {
+            File::delete($path);
+        }
+        $product->delete();
+        return redirect()->route('products.index')->with('success', 'Product Deleted Successfully!');
+
+    }
 }
