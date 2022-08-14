f25cc5b32d85002f7716ec8ac4fdaa0c24381215
jhinfex <tiachop1699@gmail.com>
2022-08-14 22:43:44 +0700

    fix css

--- resources/views/client/layouts/banner.blade.php
+++ resources/views/client/layouts/banner.blade.php
@@ -1,38 +1,7 @@
 <div class="home-bg">
    <section class="home" id="home">
-<<<<<<< HEAD
       <div class="slide-container">
  
-<<<<<<< HEAD
-       <div class="slide-container">
-         @foreach ($slider as $key => $value)
-    
-
-          <div class="slide active">
-             <div class="image">
-                <img src="{{asset('uploads/img/'.$value->image)}}" alt="">
-             </div>
-             <div class="content">
-                <h3>{{ $value->title }}</h3>
-                <div class="fas fa-angle-left" onclick="prev()"></div>
-                <div class="fas fa-angle-right" onclick="next()"></div>
-             </div>
-          </div>
-          @endforeach
-          <div class="slide">
-             <div class="image">
-                <img src="client/images/home-img-2.png" alt="">
-             </div>
-             <div class="content">
-                <h3>Pizza With Mushrooms</h3>
-                <div class="fas fa-angle-left" onclick="prev()"></div>
-                <div class="fas fa-angle-right" onclick="next()"></div>
-             </div>
-          </div>
-=======
-=======
-
->>>>>>> f8bf4901e183f4bad3a96865acd11c8a25b3c809
          <div class="slide active">
             <div class="image">
                <img src="client/images/home-img-1.png" alt="">
@@ -64,38 +33,7 @@
                <div class="fas fa-angle-left" onclick="prev()"></div>
                <div class="fas fa-angle-right" onclick="next()"></div>
             </div>
-<<<<<<< HEAD
          </div>
->>>>>>> main
-=======
-
-       <div class="slide-container">
-         @foreach ($slider as $key => $value)
-    
-
-          <div class="slide active">
-             <div class="image">
-                <img src="{{asset('uploads/img/'.$value->image)}}" alt="">
-             </div>
-             <div class="content">
-                <h3>{{ $value->title }}</h3>
-                <div class="fas fa-angle-left" onclick="prev()"></div>
-                <div class="fas fa-angle-right" onclick="next()"></div>
-             </div>
-          </div>
-          @endforeach
-          <div class="slide">
-             <div class="image">
-                <img src="client/images/home-img-2.png" alt="">
-             </div>
-             <div class="content">
-                <h3>Pizza With Mushrooms</h3>
-                <div class="fas fa-angle-left" onclick="prev()"></div>
-                <div class="fas fa-angle-right" onclick="next()"></div>
-             </div>
-          </div>
-
->>>>>>> f8bf4901e183f4bad3a96865acd11c8a25b3c809
  
       </div>
  
